13ef09bf29c1b3dc7b087ad9c4cf8d351380353a
goragodp <po.goragod@gmail.com>
2022-01-24 01:05:32 +0900

    nyan

--- dataset_generate.py
+++ dataset_generate.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+from cv2 import CC_STAT_AREA
 import numpy as np
 from glob import glob
 import math
@@ -19,6 +20,34 @@ def resizeWithAspectRatio(im, w=None, h=None, inter=cv.INTER_AREA):
 
     return cv.resize(im, dim, interpolation=inter)
 
+def generate_mask(f_img_list, f_lab_list, target_size):
+  #delete Yellow line in image
+    lower = np.array([0, 180, 250], dtype="uint8")
+    upper = np.array([100, 255, 255], dtype="uint8")
+    n = 1
+
+    for im, lab in zip(f_img_list, f_lab_list):
+        print("Start processing Image {} (label file {})".format(im, lab))
+        img_lab = cv.imread(im)
+        img_ori = cv.imread(lab)
+        
+        mask = cv.inRange(img_lab, lower, upper)
+
+        # # find  closed contour and fill
+        cnt = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        cnt = cnt[0] if len(cnt) == 2 else cnt[1]
+
+        nbr_of_cnt = 0
+        for c in cnt:
+            cv.drawContours(mask, [c], 0, (255, 255, 255), -1) # fill close area of contour
+            rect = cv.boundingRect(c)
+            if rect[2] > 100 or rect[3] > 100: nbr_of_cnt += 1
+        crop = cv.bitwise_and(img_ori, img_ori, mask=mask) #create mask area of Asago
+
+        cv.imwrite("mask%s.png" % str(n).zfill(2),resizeWithAspectRatio(mask, 800))
+        n+= 1
+        # cv.waitKey(0)
+        # cv.destroyAllWindows()
 def generate_dataset(f_img_list, f_lab_list, target_size):
     #delete Yellow line in image
     lower = np.array([0, 180, 250], dtype="uint8")
@@ -52,7 +81,10 @@ def generate_dataset(f_img_list, f_lab_list, target_size):
         print("------ Found contour {} (offset = 100px^2)".format(nbr_of_cnt))
         print("------ start generating dataset from contour")
         i = 1
- 
+
+
+        # cv.imshow("Labeled Image", resizeWithAspectRatio(img_lab, 600))
+        # cv.imshow("Mask", resizeWithAspectRatio(mask, 600))
         for c in cnt:
             # draw a rectangle
             rect = cv.boundingRect(c)
@@ -66,9 +98,15 @@ def generate_dataset(f_img_list, f_lab_list, target_size):
             # plt.subplot(211),plt.imshow(cc_img)
             # plt.subplot(212),plt.imshow(cc_msk)
             # plt.show()
+
+            # cv.imshow("crop", resizeWithAspectRatio(cc_img, 600))
+            # cv.imshow("crop_mask", resizeWithAspectRatio(cc_msk, 600))
+            # cv.waitKey(0)
+            
             div_img_and_save(cc_img, cc_msk, target_size) #Divide image accordingly to sqaure patch size *default 200 (200x200px)
             
             i+=1
+        # cv.destroyAllWindows()
 
 def cal_crop_size(w,h, target)->tuple:
     coeff_h = math.ceil(h / target)
@@ -86,36 +124,41 @@ def div_img_and_save(crop, mask, patch_size = 200):
             cc_data = crop[i:i + patch_size, j:j + patch_size, :] # all ch
             cc_mask = mask[i:i + patch_size, j:j + patch_size] # only bw
             if(cc_data.shape[0] != cc_data.shape[1] or cc_data.shape[0] < patch_size or cc_data.shape[1] < patch_size): continue
-            nn += 1
-            cv.imwrite("dataset/validation/asagao_mask_%d.png" % nn, cc_mask)
-            cv.imwrite("dataset/train/asagao_%d.png" % nn, cc_data)
+            cv.imwrite("dataset/validation/asagao_mask_%s.png" % str(nn).zfill(4), cc_mask)
+            cv.imwrite("dataset/train/asagao_%s.png" % str(nn).zfill(4), cc_data)
             print("Dataset patches {} of size {} px saved".format(nn, patch_size))
+            nn += 1
 
 if __name__ == "__main__":
     import os
     # remove previos save dataset
 
-    if len(os.listdir('dataset/train')) != 0:
-        print("Found previous dataset, attemp delete")
-        file_to_delete = os.listdir('dataset/train')
-        for f in file_to_delete:
-            os.remove('dataset/train/' + f)
+    # if len(os.listdir('dataset/train')) != 0:
+    #     print("Found previous dataset, attemp delete")
+    #     file_to_delete = os.listdir('dataset/train')
+    #     for f in file_to_delete:
+    #         os.remove('dataset/train/' + f)
     
-    if len(os.listdir('dataset/validation')) != 0:
-        print("Found previous dataset, attemp delete")
-        file_to_delete = os.listdir('dataset/validation')
-        for f in file_to_delete:
-            os.remove('dataset/validation/' + f)
+    # if len(os.listdir('dataset/validation')) != 0:
+    #     print("Found previous dataset, attemp delete")
+    #     file_to_delete = os.listdir('dataset/validation')
+    #     for f in file_to_delete:
+    #         os.remove('dataset/validation/' + f)
 
-    FILES_LABELS_IMG = sorted(glob('dataset/raw/labeled/*')) #get image with label mask
-    FILES_ORIG_IMG = list(sorted(set(glob('dataset/raw/not_labeled/*')) - set(FILES_LABELS_IMG)))
+    FILES_LABELS_IMG = sorted(glob('dataset/raw/labeled/30m-1/*')) #get image with label mask
+    FILES_ORIG_IMG = sorted(glob('dataset/raw/not_labeled/30m-1/*'))
+
+    # uncomment to include 50m data
+    # FILES_LABELS_IMG += sorted(glob('dataset/raw/labeled/50m/*')) #get image with label mask
+    # FILES_ORIG_IMG += sorted(glob('dataset/raw/not_labeled/50m/*'))
 
     if len(FILES_LABELS_IMG) > 0 and len(FILES_ORIG_IMG) > 0 and len(FILES_ORIG_IMG) == len(FILES_LABELS_IMG):
-        generate_dataset(FILES_LABELS_IMG, FILES_ORIG_IMG, 256)
+        # generate_dataset(FILES_LABELS_IMG, FILES_ORIG_IMG, 256)
+        generate_mask(FILES_LABELS_IMG, FILES_ORIG_IMG, 256)
     else:
         print("cannot find target file")
-        print(FILES_LABELS_IMG)
-        print(FILES_ORIG_IMG)
+        print(FILES_LABELS_IMG, len(FILES_LABELS_IMG))
+        print(FILES_ORIG_IMG, len(FILES_ORIG_IMG))
 
 
     
--- tiled_detiled.py
+++ tiled_detiled.py
@@ -6,39 +6,46 @@ import matplotlib.pyplot as plt
 nbr_of_tile = 0
 nbr_plot_row = 3
 nbr_plot_col = 4
-nn = 1
+
 # #input, cropped image
 def tiled_image(crop, patch_size = 200, dest = "."):
-    global nn
+    nn = 1
     for i in range(0,crop.shape[0],patch_size):
         for j in range(0,crop.shape[1],patch_size):
             cc_data = crop[i:i + patch_size, j:j + patch_size, :] # all ch
             if cc_data.shape[0] < patch_size: #patch heigth
                 padding = patch_size - cc_data.shape[0]
                 patch = np.zeros((padding ,cc_data.shape[1],3))
-                print("Heigth Stack",cc_data.shape, patch.shape, nn)    
+                # print("Heigth Stack",cc_data.shape, patch.shape, nn)    
                 cc_data = np.concatenate((cc_data, patch), axis=0)
-                print("size after pad", cc_data.shape)
             if cc_data.shape[1] < patch_size: #patch width
                 padding = patch_size - cc_data.shape[1]
                 patch = np.zeros((cc_data.shape[0] ,padding, 3))
-                print("Width Stack",cc_data.shape, patch.shape, nn)  
+                # print("Width Stack",cc_data.shape, patch.shape, nn)  
                 cc_data = np.concatenate((cc_data, patch), axis=1)
-                print("size after pad", cc_data.shape)
         
-            # plt.subplot(nbr_plot_row, nbr_plot_col, nn),plt.imshow(cc_data)
-            if(nn>99):
-                cv2.imwrite(dest + "/Tile_%d.png" % nn , cc_data)   
-            elif(nn > 9):
-                cv2.imwrite(dest + "/Tile_0%d.png" % nn , cc_data) 
-            else:
-                cv2.imwrite(dest + "/Tile_00%d.png" % nn , cc_data)        
+            # plt.subplot(nbr_plot_row, nbr_plot_col, nn),plt.imshow(cc_data)  
+            cv2.imwrite(dest + "/Tile_%s.png" % str(nn).zfill(4) , cc_data)      
             nn += 1
             # cv.imwrite("dataset/validation/asagao_mask_%d.png" % nn, cc_mask)
             # cv.imwrite("dataset/train/asagao_%d.png" % nn, cc_data)
             # print("Dataset patches {} of size {} px saved".format(nn, patch_size))
 
-# if __name__ == "__main__":
+def detiled_image(heigth, width, path_to_tile, dest = '.', tile_size = 256):
+    from glob import glob
+    file = sorted(glob(path_to_tile))
+    temp = np.zeros((math.ceil(heigth / tile_size) *tile_size, math.ceil(width / tile_size) * tile_size, 3))
+    i = 0
+    for r in range(0, temp.shape[0], tile_size):
+        for c in range(0, temp.shape[1], tile_size):
+            tile = cv2.imread(file[i])
+            temp[r:r + tile_size, c:c + tile_size,:] = tile 
+
+            i += 1
+    
+    return temp[:heigth, :width]
+
+if __name__ == "__main__":
 #     try:
 #     im = cv2.imread('dataset/test/cover1.jpg')
 #     print(im.shape)
@@ -72,5 +79,12 @@ def tiled_image(crop, patch_size = 200, dest = "."):
 #     # except Exception as e:
 #     # #     print(e)
 
-img = cv2.imread('dataset/test/DJI_20211208095147_0040.JPG')
-tiled_image(img, 256, "dataset/test/30m_tiled")
+    img1 = cv2.imread('dataset/raw/not_labeled/30m-1/DJI_20211208095102_0006.JPG')
+    tiled_image(img1, 256, "dataset/test/functiontest")
+
+    img = detiled_image(img1.shape[0], img1.shape[1], "dataset/test/functiontest/*", ".")
+    import cv2
+
+    cv2.imwrite("TEST.png", img)
+    
+
